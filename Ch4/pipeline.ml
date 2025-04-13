b67c90e27d8589e22d5e7e11bf8b6b0a8df901c3
Cai Yuanrui <caiyuanrui@outlook.com>
2025-04-13 16:52:32 +0800

    Added: 4.6 Pipelining

---
+++ Ch4/pipeline.ml
@@ -0,0 +1,8 @@
+let sum_sq n =
+  let rec loop i sum = if i > n then sum else loop (i + 1) (sum + (i * i)) in
+  loop 0 0
+
+let rec ( -- ) i j = if i > j then [] else i :: (i + 1 -- j)
+let square x = x * x
+let sum = List.fold_left ( + ) 0
+let sum_sq n = 0 -- n |> List.map square |> sum
